0f2e77b74d1822a835c06d76fff33dae1fb7b10a
edgarsousa21 <edgarraphaeldesousa@hotmail.com>
2023-12-13 17:40:09 -0300

    Adicionado o Cors

--- src/app.js
+++ src/app.js
@@ -4,10 +4,14 @@ import './database'
 import { resolve } from 'path'
 import cors from 'cors'
 
+const corsOptions = {
+  origin: 'https://code-burger-interface-deploy.vercel.app/',
+  credentials: true,
+}
 class App {
   constructor() {
     this.app = express()
-    this.app.use(cors())
+    this.app.use(cors(corsOptions))
 
     this.middlewares()
     this.routes()
