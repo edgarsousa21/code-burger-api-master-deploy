c4dc45fde42f4758efb66385894d51424a46b6f5
edgarsousa21 <edgarraphaeldesousa@hotmail.com>
2024-04-10 16:00:40 -0300

    Mongodb Restart

--- src/database/index.js
+++ src/database/index.js
@@ -28,7 +28,7 @@ class Database {
 
   mongo() {
     this.mongoConnection = mongoose.connect(
-      'mongodb://mongo:2a22hdEcf-hfHh1cd6deHe3d5D12heD-@roundhouse.proxy.rlwy.net:27545',
+      'mongodb://mongo:BPKpoWAPmdpYDuMyxCyUPcaTVeYTmpfF@viaduct.proxy.rlwy.net:19918',
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -39,4 +39,10 @@ class Database {
 
 export default new Database()
 
-// mongodb://mongo:Aa3-c6CHccACHh341HGb55DfehBbbDEC@roundhouse.proxy.rlwy.net:20587
+// conta 0: mongodb://mongo:Aa3-c6CHccACHh341HGb55DfehBbbDEC@roundhouse.proxy.rlwy.net:20587
+// conta 1: 'mongodb://mongo:bfBEB5ggfA3gbeB1cBEF5HcaDAhAe5HC@monorail.proxy.rlwy.net:40158'
+
+// conta 1.2: 'mongodb://mongo:2a22hdEcf-hfHh1cd6deHe3d5D12heD-@roundhouse.proxy.rlwy.net:27545',
+// conta 1.2: 'postgresql://postgres:G3c3g3cg*4-Ea-AD-EcE6F-5B4F34D1a@monorail.proxy.rlwy.net:46261/railway',
+
+// conta atual MongoDB: 'mongodb://mongo:BPKpoWAPmdpYDuMyxCyUPcaTVeYTmpfF@viaduct.proxy.rlwy.net:19918'
